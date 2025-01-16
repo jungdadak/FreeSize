06c626ab52b2702efeef0fd4527cb69a79f9d4dc
Andy_Lee <120154249+jungdadak@users.noreply.github.com>
2025-01-17 04:56:21 +0900

    use client

--- configs/file.config.ts
+++ configs/file.config.ts
@@ -2,17 +2,34 @@
  * fie의 형식, 크기를 리턴합니다. 소수점 2째자리까지 mb단위 표시
  */
 
+// configs/file.config.ts
+'use client';
+
+// 기본값 설정
+const DEFAULT_VALID_TYPES = ['image/jpeg', 'image/png', 'image/webp'];
+const DEFAULT_MAX_SIZE = 4 * 1024 * 1024; // 4MB
+const DEFAULT_MAX_COUNT = 10;
+
 export const FILE_CONFIG = {
-  validTypes: process.env.NEXT_PUBLIC_VALID_FILE_TYPES?.split(', ').filter(
-    Boolean
-  ) || ['image/jpeg', 'image/png', 'image/webp'],
-  maxSize: Number(process.env.NEXT_PUBLIC_MAX_FILE_SIZE) || 4 * 1024 * 1024,
+  validTypes:
+    typeof window !== 'undefined'
+      ? process.env.NEXT_PUBLIC_VALID_FILE_TYPES?.split(',') ||
+        DEFAULT_VALID_TYPES
+      : DEFAULT_VALID_TYPES,
+
+  maxSize:
+    typeof window !== 'undefined'
+      ? Number(process.env.NEXT_PUBLIC_MAX_FILE_SIZE) || DEFAULT_MAX_SIZE
+      : DEFAULT_MAX_SIZE,
+
   maxSizeInMB:
-    Number(
-      (
-        (Number(process.env.NEXT_PUBLIC_MAX_FILE_SIZE) || 4 * 1024 * 1024) /
-        1048576
-      ).toFixed(2)
-    ) || 4,
-  maxImageCount: Number(process.env.NEXT_PUBLIC_MAX_IMAGE_COUNT) || 10,
+    typeof window !== 'undefined'
+      ? Number(process.env.NEXT_PUBLIC_MAX_FILE_SIZE || DEFAULT_MAX_SIZE) /
+        (1024 * 1024)
+      : DEFAULT_MAX_SIZE / (1024 * 1024),
+
+  maxImageCount:
+    typeof window !== 'undefined'
+      ? Number(process.env.NEXT_PUBLIC_MAX_IMAGE_COUNT) || DEFAULT_MAX_COUNT
+      : DEFAULT_MAX_COUNT,
 };
